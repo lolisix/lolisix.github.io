e2ce818b7b3fe38f4920a9609569eddfbeb88db0
lolisix <mario_shencaozuo@outlook.com>
2022-08-09 17:37:58 +0800

    修改顶部导言框过长的问题

--- 三国杀子网页/调整顶部导航框长度.js
+++ 三国杀子网页/调整顶部导航框长度.js
@@ -1,4 +1,5 @@
 function 调整顶部导航框长度()//
-{   长度 = 300*document.body.clientHeight/document.body.clientWidth;
+{   长度 = 300*window.screen.availHeight/window.screen.availWidth;
+// alert(长度.toString()+"em")
     document.getElementById("顶部导言框").height = 长度.toString()+"em"
 }
