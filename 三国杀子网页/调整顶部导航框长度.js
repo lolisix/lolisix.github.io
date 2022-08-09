b8dbc4d688f445e499a556e07edf5331d0ad5521
lolisix <mario_shencaozuo@outlook.com>
2022-08-09 17:50:32 +0800

    将上限改为380em

--- 三国杀子网页/调整顶部导航框长度.js
+++ 三国杀子网页/调整顶部导航框长度.js
@@ -1,5 +1,5 @@
 function 调整顶部导航框长度()//
 {   长度 = 300*window.screen.availHeight/window.screen.availWidth;
-// alert(Math.min(500, 长度))
-    document.getElementById("顶部导言框").height = (Math.min(500, 长度)).toString()+"em"
+// alert(Math.min(380, 长度))
+    document.getElementById("顶部导言框").height = (Math.min(380, 长度)).toString()+"em"
 }
