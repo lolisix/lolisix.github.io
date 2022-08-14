d8823359ea01705fea957242c4a29745d79fd0e5
lolisix <mario_shencaozuo@outlook.com>
2022-08-14 22:07:42 +0800

    尝试修复图片在移动端上无法显示的问题

--- 三国杀子网页/图片相对路径.js
+++ 三国杀子网页/图片相对路径.js
@@ -3,7 +3,7 @@ function 图片相对路径(默认路径)//
     全图片 = document.getElementsByClassName("图片")
     for(var i=0; i<全图片.length; i++)//
     {
-        全图片[i].src=默认路径 + 全图片[i].dataset.路径
+        全图片[i].src=默认路径 + 全图片[i].dataset.路径+"?format/jpg"
     }
 }
 
