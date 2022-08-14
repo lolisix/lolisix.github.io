383df993f6fdab733f217abf8c56483ff620f637
lolisix <mario_shencaozuo@outlook.com>
2022-08-14 18:55:49 +0800

    分解图库

--- 三国杀子网页/图片相对路径.js
+++ 三国杀子网页/图片相对路径.js
@@ -1,8 +1,46 @@
-function 图片相对路径(默认路径)
+function 图片相对路径(默认路径)//
 {
     全图片 = document.getElementsByClassName("图片")
-    for(var i=0; i<全图片.length; i++)
+    for(var i=0; i<全图片.length; i++)//
     {
         全图片[i].src=默认路径 + 全图片[i].dataset.路径
+    }
+}
+
+function 图床函数(网站, 参数1)//
+{
+    if(网站="github")//
+    {
+        switch(参数1)//
+        {
+            case "手牌": 
+                return 'https://raw.githubusercontent.com/lolisix/shoupai/main/';
+            case "武将A":
+                return 'https://raw.githubusercontent.com/lolisix/wujiangA/main/';
+            case "武将B1-2":
+                return 'https://raw.githubusercontent.com/lolisix/wujiangB1-2/main/';
+            case "武将B3":
+                return 'https://raw.githubusercontent.com/lolisix/wujiangB3/main/';
+            default:
+                window.alert(参数1错误)
+                return '';
+        }
+    }
+    else if(网站="file")//
+    {
+        switch(参数1)//
+        {
+            case "手牌": 
+                return 'file:///E://个人主页/三国杀民间扩展包/手牌/';
+            case "武将A":
+                return 'file:///E://个人主页/三国杀民间扩展包/武将A/';
+            case "武将B1-2":
+                return 'file:///E://个人主页/三国杀民间扩展包/武将B1-2/';
+            case "武将B3":
+                return 'file:///E://个人主页/三国杀民间扩展包/武将B3/';
+            default:
+                window.alert(参数1错误)
+                return '';
+        }
     }
 }
